40393ee2dabcd3b7e155f573df8ab591ede82155
IvanAssis07 <ivan.assis07@gmail.com>
2023-12-08 19:17:41 -0300

    removendo caches e arquivos extras

--- tests.sh
+++ tests.sh
@@ -15,19 +15,19 @@ python3 TP1.py G 5 8 2 1 7 3 0 4 6 PRINT > logs/P12_log.txt
 python3 TP1.py G 5 8 2 0 7 3 1 4 6 PRINT > logs/P13_log.txt
 python3 TP1.py G 5 8 2 7 0 3 1 4 6 PRINT > logs/P14_log.txt
 python3 TP1.py G 8 0 2 5 7 3 1 4 6 PRINT > logs/P15_log.txt
-# python3 TP1.py G 8 7 2 5 0 3 1 4 6 PRINT > logs/P16_log.txt
-# python3 TP1.py G 5 0 8 7 3 2 1 4 6 PRINT > logs/P17_log.txt
-# python3 TP1.py G 8 7 2 5 4 3 1 6 0 PRINT > logs/P18_log.txt
-# python3 TP1.py G 8 0 7 5 3 2 1 4 6 PRINT > logs/P19_log.txt
-# python3 TP1.py G 8 7 0 5 4 2 1 6 3 PRINT > logs/P20_log.txt
-# python3 TP1.py G 8 0 7 5 4 2 1 6 3 PRINT > logs/P21_log.txt
-# python3 TP1.py G 0 8 7 5 4 2 1 6 3 PRINT > logs/P22_log.txt
-# python3 TP1.py G 8 4 7 5 6 2 1 0 3 PRINT > logs/P23_log.txt
-# python3 TP1.py G 8 4 7 5 6 2 1 3 0 PRINT > logs/P24_log.txt
-# python3 TP1.py G 8 4 7 5 6 0 1 3 2 PRINT > logs/P25_log.txt
-# python3 TP1.py G 0 4 7 8 6 2 5 1 3 PRINT > logs/P26_log.txt
-# python3 TP1.py G 8 0 7 5 4 6 1 3 2 PRINT > logs/P27_log.txt
-# python3 TP1.py G 8 4 7 6 2 3 5 1 0 PRINT > logs/P28_log.txt
-# python3 TP1.py G 8 0 6 5 7 3 1 2 4 PRINT > logs/P29_log.txt
-# python3 TP1.py G 0 8 7 5 6 4 1 2 3 PRINT > logs/P30_log.txt
-# python3 TP1.py G 8 6 7 2 5 4 3 0 1 PRINT > logs/P31_log.txt
+python3 TP1.py G 8 7 2 5 0 3 1 4 6 PRINT > logs/P16_log.txt
+python3 TP1.py G 5 0 8 7 3 2 1 4 6 PRINT > logs/P17_log.txt
+python3 TP1.py G 8 7 2 5 4 3 1 6 0 PRINT > logs/P18_log.txt
+python3 TP1.py G 8 0 7 5 3 2 1 4 6 PRINT > logs/P19_log.txt
+python3 TP1.py G 8 7 0 5 4 2 1 6 3 PRINT > logs/P20_log.txt
+python3 TP1.py G 8 0 7 5 4 2 1 6 3 PRINT > logs/P21_log.txt
+python3 TP1.py G 0 8 7 5 4 2 1 6 3 PRINT > logs/P22_log.txt
+python3 TP1.py G 8 4 7 5 6 2 1 0 3 PRINT > logs/P23_log.txt
+python3 TP1.py G 8 4 7 5 6 2 1 3 0 PRINT > logs/P24_log.txt
+python3 TP1.py G 8 4 7 5 6 0 1 3 2 PRINT > logs/P25_log.txt
+python3 TP1.py G 0 4 7 8 6 2 5 1 3 PRINT > logs/P26_log.txt
+python3 TP1.py G 8 0 7 5 4 6 1 3 2 PRINT > logs/P27_log.txt
+python3 TP1.py G 8 4 7 6 2 3 5 1 0 PRINT > logs/P28_log.txt
+python3 TP1.py G 8 0 6 5 7 3 1 2 4 PRINT > logs/P29_log.txt
+python3 TP1.py G 0 8 7 5 6 4 1 2 3 PRINT > logs/P30_log.txt
+python3 TP1.py G 8 6 7 2 5 4 3 0 1 PRINT > logs/P31_log.txt
